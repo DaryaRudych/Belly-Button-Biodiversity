a1a1c7954b825ec7fbb8406b4191d188d79f2da7
Darya Rudych <darya.rudych@gmail.com>
2018-10-28 16:19:51 -0500

    Heroku setup files

---
+++ BellyButtonBiodiversity/models.py
@@ -0,0 +1,22 @@
+from .app import db
+import os
+
+import pandas as pd
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+from flask import Flask, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(db.engine, reflect=True)
+
+# Save references to each table
+Samples_Metadata = Base.classes.sample_metadata
+Samples = Base.classes.samples
